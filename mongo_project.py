5d864d9ec3d8b91ef34678d0e67080e1842c8aae
Amyh97 <amy.hollis@btinternet.com>
2020-10-10 18:31:33 +0000

    add other crud functionality

--- mongo_project.py
+++ mongo_project.py
@@ -3,7 +3,7 @@ import os
 
 from os import path
 if path.exists("env.py"):
-    import env 
+    import env
 
 
 MONGO_URI = os.environ.get('MONGO_URI')
@@ -22,7 +22,7 @@ def mongo_connect(url):
 def show_menu():
     print('')
     print('1. Add record')
-    print('2. Find a record bt name')
+    print('2. Find a record by name')
     print('3. Update a record')
     print('4. Delete a record')
     print('5. Exit')
@@ -30,25 +30,95 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
-def add_record():
-    print('')
-    first = input('Enter first name >')
-    last = input('Enter last name >')
-    dob = input('Enter date of birth >')
-    gender = input('Enter gender >')
-    hair_colour = input('Enter hair colour >')
-    occupation = input('Enter occupation >')
-    nationality = input('Enter nationality >')
 
-    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob, 'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation, 'nationality': nationality}
+def get_record():
+    print('')
+    first = input('Enter first name> ')
+    last = input('Enter last name > ')
 
     try:
-        coll.insert_one(new_doc)
-        print('')
-        print('Document inserted')
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
     except:
         print('Error accessing the database')
 
+    if not doc:
+        print('')
+        print('Error! No results found.')
+
+    return doc 
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
+
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print('')
+        for k,v in doc.items():
+            if k != '_id':
+                print(k.capitalize() + ':' + v.capitalize())
+
+
+def edit_record():
+    doc= get_record()
+    if doc: 
+        update_doc= {}
+        print('')
+        for k,v in doc.items():
+            if k != '_id':
+                update_doc[k] = input(k.capitalize() + '[' + v + '] > ')
+
+                if update_doc[k]== '':
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print('')
+            print('Document updated')
+        except:
+            print('Error accessing the database')
+
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print('')
+        for k,v in doc.items():
+            if k != '_id':
+                print(k.capitalize() + ':' + v.capitalize())
+
+        print('')
+        confirmation = input('Is this the document you want to delete?\n Y" or "N" >')
+        print('')
+
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.delete_one(doc)
+                print('Document deleted!')
+            except:
+                print('Error accessing the database')
+        else:
+            print('Document not deleted')
 
 def main_loop():
     while True:
@@ -56,16 +126,17 @@ def main_loop():
         if option == '1':
             add_record()
         elif option == '2':
-            print('You have selected option 2')
+            find_record()
         elif option == '3':
-            print('You have selected option 3')
+            edit_record()
         elif option == '4':
-            print('You have selected option 4')
+            delete_record()
         elif option == '5':
             conn.close()
             break
         else:
             print('invalid option')
+
 
 conn = mongo_connect(MONGO_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
